f462c6b78f3780361e1a647f189305aaebccde56
emilianaAilen <emiliana.ailen@hotmail.com>
2021-11-19 00:28:27 -0300

    estilos text y varios

--- scenes/Dialog.gd
+++ scenes/Dialog.gd
@@ -13,16 +13,26 @@ onready var _Option_List = $DialogBox/Body/OptionList
 onready var _Action_Description = $DialogBox/Body/ActionBox/ActionLabel
 onready var _Speaker_Text = $DialogBox/Body/Speaker/SpeakerLabel
 onready var _dialog_sfx = $DialogSfx
+onready var _action_button = $DialogBox/Body/ActionBox
+
+
+onready var speaker_texture = $DialogBox/Body/SpeakerImage/SpeakerTexture
+onready var speaker_container = $DialogBox/Body/SpeakerImage
+onready var background = $Background
+onready var background_sound = $BackgroundSound
+onready var tween = $DialogBox/Body/TextContainer/Tween
 
 onready var _Option_Button_Scene = load("res://scenes/Option.tscn")
-onready var speaker_texture = $DialogBox/Body/Speaker/SpeakerTexture
-onready var speaker_light = $DialogBox/Body/Speaker/SpeakerLight
 
 ##sounds
 export (AudioStream) var phone
 export (AudioStream) var pressnext
+export (AudioStream) var news
 
-
+func _ready():
+	background.hide()
+	_Dialog_Box.hide()
+	
 func initialize():
 	game_data = _getDialog()
 	assert(game_data, "data not found")
@@ -30,7 +40,8 @@ func initialize():
 	_play_phase()
 
 func on_spanish():
-	_Dialog_Box.visible = true
+	_Dialog_Box.show()
+	background.show()
 	gameDataPath = "res://data/gameData.json"
 	initialize()
 
@@ -42,7 +53,12 @@ func on_english():
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept") && ! (phaseId == ""):
-		_play_phase()
+		_play_phase_if_not_tween()
+
+func _play_phase_if_not_tween():
+	if !tween.is_active():
+		 _play_phase()
+		
 
 func _getDialog() -> Array:
 	var f = File.new()
@@ -55,27 +71,33 @@ func _getDialog() -> Array:
 func _get_sounds():
 	return {
 		"phone": phone,
-		"next": pressnext
+		"next": pressnext,
+		"news": news
 	}
 	
 func _play_phase():
+	_stop_sound_if_playing()	
 	if phaseId!= "001":
-		_stop_sound_if_playing()
 		_play_sfx("next")
 	var currentPhase = game_data[phaseId]
 	if currentPhase.has("image") && currentPhase.image != '':
 		var img_url = "res://assets/textures/"+currentPhase.image
-		cargarTextureSpeaker(img_url)
+		loadSpeakerTexture(img_url)
 	else:
-		speaker_texture.hide()
-		speaker_light.hide()
+		 speaker_container.hide()
 	if currentPhase.has("sound"):
 		_play_sfx(currentPhase.sound)
+	if currentPhase.has("type_sound_back"):
+		_play_back(currentPhase.type_sound_back)
 	if currentPhase.has("choices"):
 		_Action_Description.text = "Select choice"
 		_show_options(currentPhase.choices)
 	_Speaker_Text.text = currentPhase.name
 	_Text_Body.text = currentPhase.text
+	_action_button.hide()
+	tween.interpolate_property(_Text_Body, "percent_visible", 0, 1,  currentPhase.text.length() * 0.03 ,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT) 
+	tween.start()
 	if currentPhase.has("next"):
 		phaseId = currentPhase.next
 
@@ -120,11 +142,20 @@ func _play_sfx(sound_name):
 	if sounds.has(sound_name):
 		_dialog_sfx.stream = sounds[sound_name]
 		_dialog_sfx.play()
+		
+func _play_back(sound_name):
+	background_sound.stream = sounds[sound_name]
+	if !background_sound.playing:
+		background_sound.play()
 
 
 func _on_ActionNext_pressed():
 	if ( !(phaseId == "") ):
 		_play_phase()
 
-func cargarTextureSpeaker(texture_url: String):
+func loadSpeakerTexture(texture_url: String):
 	speaker_texture.texture = load(texture_url)
+
+
+func _on_Tween_tween_completed(object, key):
+	_action_button.show()
